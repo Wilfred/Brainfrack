dfcd987da401dad3490fc859a4814a9618b9c9fb
Wilfred Hughes <me@wilfred.me.uk>
2016-01-12 21:36:12 +0000

    Update tooling.

--- clojure/brainfrack/project.clj
+++ clojure/brainfrack/project.clj
@@ -3,6 +3,7 @@
   :url "https://github.com/Wilfred/Brainfrack"
   :license {:name "Gnu Public License"
             :url "http://www.gnu.org/licenses/gpl-2.0.html"}
-  :plugins [[lein-kibit "0.0.8"]]
   :dependencies [[org.clojure/clojure "1.7.0"]]
+  :plugins [[lein-kibit "0.1.2"]
+            [cider/cider-nrepl "0.10.1"]]
   :main brainfrack.core)
