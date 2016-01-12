87c09d3e13d423db96b2822582e5352db16b84a8
Wilfred Hughes <me@wilfred.me.uk>
2016-01-12 21:36:04 +0000

    Use latest clojure.

--- clojure/brainfrack/project.clj
+++ clojure/brainfrack/project.clj
@@ -3,6 +3,6 @@
   :url "https://github.com/Wilfred/Brainfrack"
   :license {:name "Gnu Public License"
             :url "http://www.gnu.org/licenses/gpl-2.0.html"}
-  :dependencies [[org.clojure/clojure "1.5.1"]]
   :plugins [[lein-kibit "0.0.8"]]
+  :dependencies [[org.clojure/clojure "1.7.0"]]
   :main brainfrack.core)
