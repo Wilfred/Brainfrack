bc2c495f5dfffd99c0512b04c7acf348be6e2f62
Wilfred Hughes <me@wilfred.me.uk>
2016-05-24 00:33:52 +0200

    Skeleton interpreter loop.

--- common_lisp/brainfrack.lisp
+++ common_lisp/brainfrack.lisp
@@ -7,7 +7,14 @@
   "Execute PROGRAM written in BF."
   (let ((program-index 0)
         (memory-index 0))
-    (format t "TODO~%")))
+    (loop while (< program-index (length program)) do
+         (case (elt program program-index)
+           (#\+ (incf (elt *bf-memory* memory-index))
+                (incf program-index))
+           (#\- (decf (elt *bf-memory* memory-index))
+                (incf program-index))
+           ;; Skip over comments.
+           (t (incf program-index))))))
 
 (defun bf-matching-brackets (program)
   "Return a hash-table of opening bracket indices to closing bracket indices
