1d34172c94bd57e5b598f0a04daf0b2bcaa67d5d
Wilfred Hughes <me@wilfred.me.uk>
2016-05-24 00:01:12 +0200

    Remove debugging notes

--- common_lisp/brainfrack.lisp
+++ common_lisp/brainfrack.lisp
@@ -18,17 +18,6 @@ Example:
 {0: 3, 1: 2}"
   (let ((matches (make-hash-table)))
     (loop for char across program
-          for index from 0
-          collect (+ 1 index)
-          )
-    ))
+       for index from 0
+       collect (+ 1 index))))
 
-(loop for char across "abc" for index from 0
-      do (progn
-           (format t "~s ~s~%" char index)))
-
-(eql #\a #\a)
-
-
-(defun foo ()
-  (format t "Hello World!~%"))
