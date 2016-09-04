ec38e8cf8bfd590a4183a126aabbbd27026d47dd
Wilfred Hughes <me@wilfred.me.uk>
2016-09-04 12:17:53 -0400

    Use a normal docstring

--- scheme/brainfrack.scm
+++ scheme/brainfrack.scm
@@ -1,7 +1,7 @@
 
-;; Given a brainfuck PROGRAM (a string), return a list of chars that
-;; are valid brainfuck instructions.
 (define (instructions program)
+  "Given a brainfuck PROGRAM (a string), return a list of chars that
+are valid brainfuck instructions."
   (filter
    (lambda (char) (memq char (list #\+ #\- #\< #\> #\. #\, #\[ #\])))
    (string->list program)))
