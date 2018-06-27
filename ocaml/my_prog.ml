8d9e428af00d5ee390c1c84e0e351b40e7475550
Wilfred Hughes <me@wilfred.me.uk>
2018-06-27 18:26:06 +0100

    Expanding ocaml skeleton

--- ocaml/my_prog.ml
+++ ocaml/my_prog.ml
@@ -10,9 +10,41 @@ let matching_close prog open_idx =
         worker (i + 1) (depth + 1)
       else
         worker (i + 1) depth
-  in 
+  in
   worker (open_idx + 1) 0;;
 
-matching_close "[foo[] ]" 0;;
+let matching_open prog close_idx =
+  let rec worker i depth =
+    if (String.get prog i) == '[' then
+      if depth == 0 then
+        i
+      else
+        worker (i - 1) (depth - 1)
+    else
+      if (String.get prog i) == ']' then
+        worker (i - 1) (depth + 1)
+      else
+        worker (i - 1) depth
+  in
+  worker (close_idx - 1) 0;;
+
+let eval_bf prog =
+  (* TODO: use a bigger cell array.*)
+  let cells = (Array.make 1000 0) in
+  let rec loop cell_idx instr_idx =
+    if instr_idx > String.length prog then
+      ()
+    else
+      let instr = (String.get prog instr_idx) in
+      match instr with
+        '+' ->
+         let cell_val = Array.get cells cell_idx in
+         Array.set cells cell_idx (cell_val + 1)
+      | '-' ->
+         let cell_val = Array.get cells cell_idx in
+         Array.set cells cell_idx (cell_val - 1)
+      | _ -> loop cell_idx (instr_idx + 1)
+  in
+  loop 0 0;;
 
 let () = print_endline "hello world"
